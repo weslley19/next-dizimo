2313f08940dccae9b91a4c1864ac9bca4ad92fe9
Weslley Oliveira <weslley.pereira@sigcorp.com.br>
2024-05-15 02:16:30 -0300

    feat: estilizando header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,18 @@
 import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Header() {
+  const user = 'Weslley'
+
   return (
     <>
-      <header className="h-24 bg-header border-b px-3 py-3 border-custom">
-        Header
-        <ThemeToggle />
+      <header className="h-26 bg-header">
+        <div className="border-b border-custom px-3 py-3
+          flex justify-between items-center
+        ">
+          <span>{user}</span>
+          <ThemeToggle />
+        </div>
+        <div className="border-b border-custom px-3 py-3">...</div>
       </header>
     </>
   )
