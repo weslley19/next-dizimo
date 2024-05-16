5355655e78c4031c5f48049227f775fa08e11b1c
Weslley Oliveira <weslley.pereira@sigcorp.com.br>
2024-05-16 12:26:47 -0300

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 export default function middleware(request: NextRequest) {
   const token = request.cookies.get('authjs.session-token')
+
   const signinURL = new URL('/signin', request.url)
   const dashboardURL = new URL('/', request.url)
 
