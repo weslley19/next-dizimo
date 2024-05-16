4bba5d0298e516904e57a8a17b863d62c6ea6d7f
Weslley Oliveira <weslley.pereira@sigcorp.com.br>
2024-05-16 20:36:50 -0300

    feat: pagina not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default async function NotFound() {
+  return (
+    <>
+      <div className="w-svw	h-svh flex flex-col justify-center items-center bg-content">
+        <h1 className="text-[6rem]">404</h1>
+        <h3 className="text-[2rem] mb-2">Página não encontrada</h3>
+        <p className="mb-10">Você veio para uma página que não existe</p>
+
+        <Link href="/">
+          <Button>Voltar para Home</Button>
+        </Link>
+      </div>
+    </>
+  )
+}
