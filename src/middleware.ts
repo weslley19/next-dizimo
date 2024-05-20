69d1e804d8c99fec5ab5d80c1d047be0234f9172
Weslley Oliveira <weslley.pereira@sigcorp.com.br>
2024-05-19 22:55:04 -0300

    fix: testando deploy vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export default function middleware(request: NextRequest) {
-  const token = request.cookies.get('authjs.session-token')
+  const token = request.cookies.get('authjs.session-token' ?? '_vercel_jwt')
 
   const signinURL = new URL('/signin', request.url)
   const dashboardURL = new URL('/', request.url)
